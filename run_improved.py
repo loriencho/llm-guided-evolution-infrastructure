94e89f9d9a68270c32cb12af9a3bb81a734ba3cf
Moses Adewolu <69429336+MosesTheRedSea@users.noreply.github.com>
2025-06-30 16:15:49 -0400

    Update run_improved.py

--- run_improved.py
+++ run_improved.py
@@ -14,34 +14,34 @@ from src.utils.print_utils import print_population, print_scores, box_print, pri
 from src.llm_utils import split_file, retrieve_base_code, mutate_prompts
 from src.cfg.constants import *
 
-def print_ancestery(data):
+def print_ancestry(data):
     for gene in data.keys():
         print(f'gene: {gene}')
         print(f"\t{data[gene]['GENES']}")
         print(f"\t{data[gene]['MUTATE_TYPE']}")
 
-def update_ancestry(gene_id_child, gene_id_parent, ancestery, mutation_type=None, gene_id_parent2=None):
+def update_ancestry(gene_id_child, gene_id_parent, ancestry, mutation_type=None, gene_id_parent2=None):
     """
     Updates the ancestry data for a given child gene based on its parent(s).
     :param gene_id_child: The ID of the child gene.
     :param gene_id_parent: The ID of the first parent gene.
-    :param ancestery: The global data structure for ancestry.
+    :param ancestry: The global data structure for ancestry.
     :param mutation_type: The type of mutation (for the first part of the code). Default is None.
     :param gene_id_parent2: The ID of the second parent gene (for the second part of the code). Default is None.
     """
     # Common part for both functionalities
-    ancestery[gene_id_child] = copy.deepcopy(ancestery[gene_id_parent])
+    ancestry[gene_id_child] = copy.deepcopy(ancestry[gene_id_parent])
     # Handle the specifics for either part 1 or part 2
     if gene_id_parent2 is None:
         # Part 1 functionality
-        ancestery[gene_id_child]['GENES'] = copy.deepcopy(ancestery[gene_id_parent]['GENES']) + [gene_id_child]
-        ancestery[gene_id_child]['MUTATE_TYPE'] = copy.deepcopy(ancestery[gene_id_parent]['MUTATE_TYPE']) + [mutation_type]
+        ancestry[gene_id_child]['GENES'] = copy.deepcopy(ancestry[gene_id_parent]['GENES']) + [gene_id_child]
+        ancestry[gene_id_child]['MUTATE_TYPE'] = copy.deepcopy(ancestry[gene_id_parent]['MUTATE_TYPE']) + [mutation_type]
     else:
         # Part 2 functionality
         cross_id = f'P:{gene_id_parent2}-C:{gene_id_child}'
-        ancestery[gene_id_child]['GENES'] = copy.deepcopy(ancestery[gene_id_parent]['GENES']) + [cross_id]
-        ancestery[gene_id_child]['MUTATE_TYPE'] = copy.deepcopy(ancestery[gene_id_parent]['MUTATE_TYPE']) + ["CrossOver"]
-    return ancestery
+        ancestry[gene_id_child]['GENES'] = copy.deepcopy(ancestry[gene_id_parent]['GENES']) + [cross_id]
+        ancestry[gene_id_child]['MUTATE_TYPE'] = copy.deepcopy(ancestry[gene_id_parent]['MUTATE_TYPE']) + ["CrossOver"]
+    return ancestry
 
 
 def generate_template(PROB_EOT, GEN_COUNT, TOP_N_GENES, SOTA_ROOT, SEED_NETWORK, ROOT_DIR):
@@ -93,7 +93,7 @@ def write_bash_script(input_filename_x=f'{SOTA_ROOT}/{SEED_NETWORK}',
     
     def fetch_gene(filepath):
         return os.path.basename(filepath).replace(f'{MODEL}_','').replace('.py','')
-    global GLOBAL_DATA_ANCESTERY
+    global GLOBAL_DATA_ANCESTRY
     QC_CHECK_BOOL = PROB_QC > np.random.uniform()
     # Extract the directory path from the file path
     dir_path = os.path.dirname(output_filename)
@@ -105,9 +105,8 @@ def write_bash_script(input_filename_x=f'{SOTA_ROOT}/{SEED_NETWORK}',
         template_txt, mute_type = generate_template(PROB_EOT, GEN_COUNT, TOP_N_GENES, 
                                                     SOTA_ROOT, SEED_NETWORK, ROOT_DIR)
         if GEN_COUNT >= 0: # this does not need to happen at creation of population
-            GLOBAL_DATA_ANCESTERY = update_ancestry(gene_id_child, gene_id_parent, GLOBAL_DATA_ANCESTERY, 
+            GLOBAL_DATA_ANCESTRY = update_ancestry(gene_id_child, gene_id_parent, GLOBAL_DATA_ANCESTRY, 
                                                     mutation_type=mute_type, gene_id_parent2=None)
-            # print(gene_id_child); print(GLOBAL_DATA_ANCESTERY[gene_id_parent])
         out_dir = str(GENERATION)
         file_path = os.path.join(out_dir, f'{gene_id_child}_model.txt')
         os.makedirs(out_dir, exist_ok=True)
@@ -117,7 +116,7 @@ def write_bash_script(input_filename_x=f'{SOTA_ROOT}/{SEED_NETWORK}',
         python_runline = f"python {temp_text} --apply_quality_control '{QC_CHECK_BOOL}' --hugging_face {HUGGING_FACE_BOOL}"
     elif python_file=='src/llm_crossover.py':
         gene_id_parent2 = fetch_gene(input_filename_y)
-        GLOBAL_DATA_ANCESTERY = update_ancestry(gene_id_child, gene_id_parent, GLOBAL_DATA_ANCESTERY, 
+        GLOBAL_DATA_ANCESTRY = update_ancestry(gene_id_child, gene_id_parent, GLOBAL_DATA_ANCESTRY, 
                                                 mutation_type=None, gene_id_parent2=gene_id_parent2)
         
         temp_text = f"{python_file} {input_filename_x} {input_filename_y} {output_filename} --top_p {top_p} --temperature {temperature}"
@@ -250,7 +249,7 @@ def create_individual(container, temp_min=0.05, temp_max=0.4):
                                             top_p=0.1, temperature=temperature)
     GLOBAL_DATA[gene_id] = {'sub_flag':successful_sub_flag, 'job_id':job_id, 
                             'status':'subbed file', 'fitness':None, 'start_time':time.time()}
-    GLOBAL_DATA_ANCESTERY[gene_id] = {'GENES':[gene_id], 'MUTATE_TYPE':["CREATED"]}
+    GLOBAL_DATA_ANCESTRY[gene_id] = {'GENES':[gene_id], 'MUTATE_TYPE':["CREATED"]}
     
     individual = container([gene_id])  # Assign a file ID
     
@@ -485,7 +484,6 @@ def update_individual(ind, new_gene_id, old_gene_id=None, process_success=True, 
         if old_gene_id is not None and old_gene_id in GLOBAL_DATA.keys():
             del GLOBAL_DATA[old_gene_id]
         print(f'\t☑ {operation}: {new_gene_id}')
-        # GLOBAL_DATA_ANCESTERY[new_gene_id] = {'SCORES':[], 'GENES':[], 'CROSS_OVERS':{}, 'MUTATE_TYPE':[]}
     else:
         print(f'\t☠ Failed {operation}: {new_gene_id}')
         if new_gene_id in GLOBAL_DATA.keys():
@@ -714,7 +712,7 @@ def save_checkpoint(gen, folder_name="checkpoints"):
         "GLOBAL_DATA_HIST": GLOBAL_DATA_HIST,
         "population": population,
         "hof": hof,
-        "GLOBAL_DATA_ANCESTERY":GLOBAL_DATA_ANCESTERY,
+        "GLOBAL_DATA_ANCESTRY":GLOBAL_DATA_ANCESTRY,
     }
     filename = os.path.join(folder_name, f'checkpoint_gen_{gen}.pkl')
     with open(filename, 'wb') as file:
@@ -774,7 +772,7 @@ TOP_N_GENES = None
 LINKED_GENES = {}
 GLOBAL_DATA = {}
 GLOBAL_DATA_HIST = {}
-GLOBAL_DATA_ANCESTERY = {}
+GLOBAL_DATA_ANCESTRY = {}
 
 # Main Evolution Loop
 if __name__ == "__main__":
@@ -792,7 +790,7 @@ if __name__ == "__main__":
         box_print("LOADING CHECKPOINT")
         GLOBAL_DATA = checkpoint["GLOBAL_DATA"]
         GLOBAL_DATA_HIST = checkpoint["GLOBAL_DATA_HIST"]
-        GLOBAL_DATA_ANCESTERY = checkpoint["GLOBAL_DATA_ANCESTERY"]
+        GLOBAL_DATA_ANCESTRY = checkpoint["GLOBAL_DATA_ANCESTRY"]
         population = checkpoint["population"]
         hof = checkpoint["hof"]
     else:
@@ -809,7 +807,6 @@ if __name__ == "__main__":
         ind.fitness.values = PLACEHOLDER_FITNESS
         
     check_and_update_fitness(population)
-    # print_ancestery(GLOBAL_DATA_ANCESTERY)
     # Evolution
     for gen in range(start_gen, num_generations):
         GEN_COUNT = gen
@@ -849,8 +846,6 @@ if __name__ == "__main__":
         offspring = list(map(toolbox.clone, offspring))
         GLOBAL_DATA_HIST.update(GLOBAL_DATA.copy())
 
-        # box_print(f"GLOBAL_DATA_ANCESTERY", new_line_end=False)
-        # print_ancestery(GLOBAL_DATA_ANCESTERY)
         # Apply crossover on the offspring
         box_print("Mating", print_bbox_len=60, new_line_end=False)
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
@@ -858,9 +853,6 @@ if __name__ == "__main__":
                 child1, child2 = toolbox.mate(child1, child2)
                 del child1.fitness.values
                 del child2.fitness.values 
-                
-        # box_print(f"GLOBAL_DATA_ANCESTERY", new_line_end=False)
-        # print_ancestery(GLOBAL_DATA_ANCESTERY)
                 
         box_print("Batch Checking Mated Genes", print_bbox_len=60, new_line_end=False)       
         offspring = delayed_mate_check(offspring)
@@ -873,8 +865,8 @@ if __name__ == "__main__":
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
                 
-        box_print(f"GLOBAL_DATA_ANCESTERY", new_line_end=False)
-        print_ancestery(GLOBAL_DATA_ANCESTERY)
+        box_print(f"GLOBAL_DATA_ANCESTRY", new_line_end=False)
+        print_ancestry(GLOBAL_DATA_ANCESTRY)
                 
         box_print("Batch Checking Mutated Genes", print_bbox_len=60, new_line_end=False)
         offspring = delayed_mutate_check(offspring)
