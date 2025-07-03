33f4b14e359dd858a3f150e2d15e240894ec2da7
Moses Adewolu <69429336+MosesTheRedSea@users.noreply.github.com>
2025-07-03 12:33:24 -0400

    Update constants.py

--- src/cfg/constants.py
+++ src/cfg/constants.py
@@ -13,6 +13,7 @@ TRAIN_FILE = os.path.join(SOTA_ROOT, "train_cls.py")
 
 CLUSTER = "pace-ice"
 LLM_MODEL = 'gemini'
+LOCAL_LLM = True
 ENVIRONMENT_DIR = "LLM-Guided-Evolution-Generic/.venv"
 SLURM_CONFIG_DIR = "LLM-Guided-Evolution-Generic/slurm-config"
 
