1041a6a0d0413b24a025894c5c1d732e4e2e96a7
Moses Adewolu <69429336+MosesTheRedSea@users.noreply.github.com>
2025-08-15 15:40:49 -0400

    Update slurm.py

--- slurm.py
+++ slurm.py
@@ -4,21 +4,27 @@ import yaml
 from src.cfg import constants
 
 def replace_script_configuration(file_path, new_config):
+        
     with open(file_path, 'w') as f:
         f.write(new_config if new_config.endswith('\n') else new_config + '\n')
 
-def save_to_yaml(llm, python, gpu, file_path=constants.SLURM_CONFIG_DIR):
+def save_to_yaml(llm, python, gpu, local_llm_server, file_path=constants.SLURM_CONFIG_DIR):
+
         yaml_data = {"gpu_selection":gpu, "python_bash_script":python,  "llm_bash_script":llm}
         
         with open(f'{file_path}/slurm_config.yaml', 'w') as f:
                 yaml.dump(yaml_data, f, indent=4)
 
 if __name__ == "__main__":
+
     configuration_path = f'{constants.SLURM_CONFIG_DIR}{constants.CLUSTER}.txt'
 
     with open(configuration_path, 'r') as file:
+
         content = [item.strip() for item in file.readlines()]
+
         indices = [index for index, value in enumerate(content) if value == "------"]
+
         runsh_config_lines = "\n".join(content[indices[0]+1:indices[1]])
 
         run_sh = f"""echo "launching LLM Guided Evolution"
@@ -29,12 +35,9 @@ export CUDA_VISIBLE_DEVICES=0
 export MKL_THREADING_LAYER=GNU 
 export SERVER_HOSTNAME=$(hostname)
 source {constants.ENVIRONMENT_DIR}/bin/activate
-{"#" if not constants.LOCAL_LLM else ""}uvicorn server:app --host $SERVER_HOSTNAME --port 8000 --workers 1 & sleep 30
 python run_improved.py point_transformers_test
 """
         replace_script_configuration("run.sh", runsh_config_lines + run_sh)
-
-        # print(runsh_config_lines + run_sh)
 
         mixtsh_config_lines = "\n".join(content[indices[2]+1:indices[3]])
         
@@ -49,8 +52,6 @@ export TOKENIZERS_PARALLELISM=false
 python llm_crossover.py '{constants.SEED_NETWORK}' '{constants.SOTA_ROOT}/models/Menghao/model_x.py' '{constants.SOTA_ROOT}/models/Menghao/model_z.py'  --top_p 0.15   --temperature 0.1 --apply_quality_control 'True' --bit 8
 """
         replace_script_configuration("src/mixt.sh", mixtsh_config_lines + mixt_sh)
-
-       # print(mixtsh_config_lines + mixt_sh)
 
         llm_gpu = content[indices[4]+1:indices[5]][0]
 
@@ -86,4 +87,24 @@ source {constants.ENVIRONMENT_DIR}/bin/activate
 # Run Python script
 {{}}
 """     
-        save_to_yaml(llm_script, python_script, llm_gpu)
+
+        local_llm_server = f"""
+echo "launching LLM Server"
+hostname
+module load cuda/12.2.2
+source {constants.ENVIRONMENT_DIR}/bin/activate
+export SERVER_HOSTNAME=$(hostname)
+HOSTNAME_FILE={constants.HOSTNAME_DIR}
+echo "Writing server hostname '$SERVER_HOSTNAME' to file: $HOSTNAME_FILE"
+echo "$SERVER_HOSTNAME" > "$HOSTNAME_FILE"
+echo "Starting LLM server on host: $SERVER_HOSTNAME"
+uvicorn server:app --host $SERVER_HOSTNAME --port 8000 --workers 1 --no-access-log 
+"""           
+
+        local_llm_server_config = "\n".join(content[indices[10]+1:indices[11]])
+
+        print(local_llm_server_config)
+
+        replace_script_configuration("server.sh", local_llm_server_config + local_llm_server)
+
+        save_to_yaml(llm_script, python_script, llm_gpu, local_llm_server)
