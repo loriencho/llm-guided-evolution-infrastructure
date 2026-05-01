95d11ca1a037cf2316ebd04fd418df5a0c4efa27
Lorien Cho <42721802+loriencho@users.noreply.github.com>
2026-05-01 15:09:15 -0400

    Update submodule_add.sh

--- submodule_add.sh
+++ submodule_add.sh
@@ -1,7 +1,8 @@
 SUBMODULE_PATH="sota/ultralytics"
 SPARSE_PATH="ultralytics/cfg"
+SUBMODULE_REPO="https://github.com/jasonzutty/ultralytics.git"
 
-git submodule add https://github.com/jasonzutty/ultralytics.git  "$SUBMODULE_PATH"
+git submodule add "$SUBMODULE_REPO" "$SUBMODULE_PATH"
 cd "$SUBMODULE_PATH"
 git submodule update --init --filter=blob:none
 git sparse-checkout init cone
